fbcb125e3375f77268726434ae877c8b5de83aed
MyUsername <romamargine@gmail.com>
2022-03-24 15:38:45 +0100

    initial commit

---
+++ js/main.js
@@ -0,0 +1,22 @@
+/* 
+chiedere all'utente il numero di chilometri che vuole percorrere e l'età del passeggero.
+il prezzo del biglietto è definito in base ai km (0.21 € al km)
+va applicato uno sconto del 20% per i minorenni
+va applicato uno sconto del 40% per gli over 65. 
+*/
+
+//chiedere all'utente l'età
+
+
+
+//chiedere all'utente il numero di km che vuole percorrere
+
+
+
+//prezzo del biglietto in base ai km
+
+
+//sconto del 20% per minorenni
+
+
+//sconto 40% per gli over 65
